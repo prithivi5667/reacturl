b914f0af3ed7808c1ddc8923f12e30950c2f939f
prithivi5667 <prithivi5667@gmail.com>
2021-12-20 12:22:59 +0530

    new config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "https://eloquent-mcclintock-044a3c.netlify.app"
+    baseURL: "https://shortner-urlpt.herokuapp.com"
 })  
